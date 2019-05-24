7ecd7ba833c499a2f8b5aee1d07398d588397b4f
WataruSuzuki <wataru0406@gmail.com>
2019-05-24 22:40:00 +0900

    Created xcode project

---
+++ ClockInOut/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ClockInOut
+//
+//  Created by 鈴木航 on 2019/09/03.
+//  Copyright © 2019 WataruSuzuki. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ClockInOut/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ClockInOut
+//
+//  Created by 鈴木航 on 2019/09/03.
+//  Copyright © 2019 WataruSuzuki. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
