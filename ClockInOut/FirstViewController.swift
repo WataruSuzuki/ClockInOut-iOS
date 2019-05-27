a00fb606c81c4f8fbb8c17d810db2ed258e9e250
WataruSuzuki <wataru0406@gmail.com>
2019-05-27 22:20:00 +0900

    Added location delegate methods

--- ClockInOut/FirstViewController.swift
+++ ClockInOut/FirstViewController.swift
@@ -7,14 +7,67 @@
 //
 
 import UIKit
+import CoreLocation
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController,
+    CLLocationManagerDelegate
+{
+    
+    private var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        locationManager.delegate = self
+        
+        handleAuthorizationStatus(status: CLLocationManager.authorizationStatus())
+    }
+    
+    private func handleAuthorizationStatus(status: CLAuthorizationStatus) {
+        switch status {
+        case .denied: fallthrough
+        case .authorizedWhenInUse: fallthrough
+        case .restricted:
+            break
+        case .notDetermined:
+            locationManager.requestAlwaysAuthorization()
+        case .authorizedAlways:
+            locationManager.startUpdatingLocation()
+        @unknown default:
+            fatalError()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        handleAuthorizationStatus(status: status)
     }
 
-
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(#function)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(#function)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didStartMonitoringFor region: CLRegion) {
+        print(#function)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        print(#function)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+        print(#function)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didDetermineState state: CLRegionState, for region: CLRegion) {
+        print(#function)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, monitoringDidFailFor region: CLRegion?, withError error: Error) {
+        print(#function)
+    }
 }
 
